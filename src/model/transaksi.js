716e8291f4987170bd84a6d1c2652825afe0f788
aditbagja <aditbagja44@gmail.com>
2024-05-07 08:56:48 +0700

    fix model

--- src/model/transaksi.js
+++ src/model/transaksi.js
@@ -1,27 +1,33 @@
 const mongoose = require("mongoose");
 
-const transaksiSchema = new mongoose.Schema({
-  qrCode: {
-    require: true,
-    type: String,
+const transaksiSchema = new mongoose.Schema(
+  {
+    qrCode: {
+      require: true,
+      type: String,
+    },
+    rfid: {
+      require: true,
+      type: String,
+    },
+    hargaSatuan: {
+      require: true,
+      type: Number,
+    },
+    jumlah: {
+      require: true,
+      type: Number,
+    },
+    waktuPesan: {
+      type: Date,
+      default: Date.now(),
+    },
   },
-  rfid: {
-    require: true,
-    type: String,
-  },
-  hargaSatuan: {
-    require: true,
-    type: Number,
-  },
-  jumlah: {
-    require: true,
-    type: Number,
-  },
-  waktuPesan: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  {
+    collection: "transaksi",
+    versionKey: false,
+  }
+);
 
 const Transaksi = mongoose.model("transaksi", transaksiSchema);
 
